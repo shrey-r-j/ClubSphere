1d4506b3b10af073d5adb9ec0f51d1e3d6e5c350
TechDivs <divyanshkhatikar@gmail.com>
2025-02-04 16:00:05 +0530

    Initial Commit

---
+++ ClubSphere/src/pages/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const location = useLocation();
+  const isOnClubsPage = location.pathname === "/student/clubs";
+
+  return (
+    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center shadow-md">
+      <div className="flex items-center">
+        <img src={logo} alt="ClubSphere Logo" className="h-10 w-10 mr-2" />
+        <span className="text-xl font-semibold">ClubSphere</span>
+      </div>
+
+      <div>
+        <NavLink
+          to={isOnClubsPage ? "/student" : "/student/clubs"}
+          className={({ isActive }) => `text-lg mx-4 ${isActive ? "text-blue-400" : "text-white"}`}
+        >
+          {isOnClubsPage ? "Feed" : "Clubs"}
+        </NavLink>
+      </div>
+
+      {/* User Icon */}
+      <div>
+        <NavLink to="/student/dashboard">
+          <FaUserCircle className="text-3xl cursor-pointer hover:text-blue-400" />
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
